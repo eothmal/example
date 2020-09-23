3de9d745f5b462bbd5da60685b82d95cbc4f1c06
eothmal@att.net <eothmal@att.net>
2020-09-22 22:14:10 -0500

    update

--- javaeight/src/com/sample/dstream/StringBlankSupperss.java
+++ javaeight/src/com/sample/dstream/StringBlankSupperss.java
@@ -12,14 +12,10 @@ public class StringBlankSupperss {
 
         char[] ch = before.toCharArray();
         int len = before.length();
-        char[] chr = new char[len];
         StringBuffer buffer = new StringBuffer();
-        int i=0;
         for (int n = 0; n < len; n++){
             if (ch[n] != ' '){
-                chr[i] = ch[n];
-                buffer.append(chr[i]);
-                i++;
+                buffer.append(ch[n]);
             }
         }
         System.out.println("wo stream: " + buffer);
