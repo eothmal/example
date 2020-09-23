b70c2014cd67d60e9bcb8d752342d76f92926595
eothmal@att.net <eothmal@att.net>
2020-09-22 22:09:53 -0500

    update

--- javaeight/src/com/sample/dstream/AggregateOperationICollector.java
+++ javaeight/src/com/sample/dstream/AggregateOperationICollector.java
@@ -9,12 +9,12 @@ public class AggregateOperationICollector {
 
         Collection<Person> collection = new ArrayList<>();
         collection.add(new Person("Amir", Person.Gender.Male, 26, "Seattle"));
-        collection.add(new Person("Bakar", Person.Gender.Male, 42, "New York"));
-        collection.add(new Person("Cici", Person.Gender.Female, 35, "San Fransisco"));
+        collection.add(new Person("Baker", Person.Gender.Male, 42, "New York"));
+        collection.add(new Person("Claire", Person.Gender.Female, 35, "San Fransisco"));
         collection.add(new Person("Darius", Person.Gender.Male, 32, "San Jose"));
         collection.add(new Person("Esmeralda", Person.Gender.Female, 24, "Salt Lake City"));
         collection.add(new Person("Ferry", Person.Gender.Male, 45, "Boston"));
-        collection.add(new Person("Gagu", Person.Gender.Male, 39, "New York"));
+        collection.add(new Person("George", Person.Gender.Male, 39, "New York"));
         collection.add(new Person("Hasan", Person.Gender.Male, 29, "Seattle"));
         collection.add(new Person("Irina", Person.Gender.Female, 43, "Boston"));
         collection.add(new Person("Julia", Person.Gender.Female, 29, "New York"));
--- javaeight/src/com/sample/dstream/StringBlankSupperss.java
+++ javaeight/src/com/sample/dstream/StringBlankSupperss.java
@@ -1,6 +1,7 @@
 package com.sample.dstream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,14 +10,28 @@ public class StringBlankSupperss {
     public static void main(String[] args) {
         String before = "abc defg xyz";
 
-        String ss =
+        char[] ch = before.toCharArray();
+        int len = before.length();
+        char[] chr = new char[len];
+        StringBuffer buffer = new StringBuffer();
+        int i=0;
+        for (int n = 0; n < len; n++){
+            if (ch[n] != ' '){
+                chr[i] = ch[n];
+                buffer.append(chr[i]);
+                i++;
+            }
+        }
+        System.out.println("wo stream: " + buffer);
+
+        String after =
                 before.chars()
                         .mapToObj(e -> (char) e)
                         .filter(character -> character != ' ')
                         .collect(Collectors.toList())
                         .stream().map(e -> e.toString()).reduce((accStr, s) -> accStr + s).get();
 
-        System.out.println(ss);
+        System.out.println("w/ stream: " + after);
 
     }
 }
