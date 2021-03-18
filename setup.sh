5a70f95639aaf8a9a0a8c1fe6f185e8b3280aca1
eothmal <eothmal@att.net>
2021-03-18 02:38:13 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # find . -name '*.java' > cscope.files
 # find . -name '*.html' >> cscope.files
+# find /my/project/dir -name '*.c' -o -name '*.h' > /foo/cscope.files
 mkdir -p ~/.vim/plugin && \
 cp *.vim ~/.vim/plugin/. && \
 cscope -b
